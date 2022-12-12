331910e430dfc945eb80bd0b10e8c55be12ef8df
Saikrishna-Chaitanya-CH <krishnachaitu616@gmail.com>
2022-12-12 07:12:48 +0000

    first commit

---
+++ src/components/Faqs/index.js
@@ -0,0 +1,22 @@
+import {Component} from 'react'
+import FaqItem from '../FaqItem'
+import './index.css'
+
+class Faqs extends Component {
+  render() {
+    const {faqsList} = this.props
+    return (
+      <div className="app-container">
+        <div className="faqs-container">
+          <h1 className="heading">FAQS</h1>
+          <ul className="faqs-list-container">
+            {faqsList.map(eachFaq => (
+              <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Faqs
